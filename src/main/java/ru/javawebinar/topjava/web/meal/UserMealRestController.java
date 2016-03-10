9af22113ea60c41ab78cd0b760daee1b0bf7f6ea
Asset Uisin <jobs.uisin@gmail.com>
2016-03-10 17:29:37 +0600

    User&Meal controllers

---
+++ src/main/java/ru/javawebinar/topjava/web/meal/UserMealRestController.java
@@ -0,0 +1,10 @@
+package ru.javawebinar.topjava.web.meal;
+
+import ru.javawebinar.topjava.service.UserMealServiceImpl;
+
+/**
+ * Created by Asset on 10.03.2016.
+ */
+public class UserMealRestController {
+    private UserMealServiceImpl service;
+}
