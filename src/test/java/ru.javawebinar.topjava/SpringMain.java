325d802f7395c812822fa50ae157cad7fe0594ff
Asset Uisin <jobs.uisin@gmail.com>
2016-06-30 12:20:17 +0600

    create-mock-test-ctx

--- src/test/java/ru.javawebinar.topjava/SpringMain.java
+++ src/test/java/ru.javawebinar.topjava/SpringMain.java
@@ -18,7 +18,7 @@ public class SpringMain {
     public static void main(String[] args) {
         // java 7 Automatic resource management
         //try (AnnotationConfigApplicationContext appCtx = new AnnotationConfigApplicationContext(AppConfig.class)) {
-        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
+        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml","spring/mock.xml")) {
                 System.out.println(Arrays.toString(appCtx.getBeanDefinitionNames()));
                 AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
                 System.out.println(adminUserController.create(UserTestData.USER));
